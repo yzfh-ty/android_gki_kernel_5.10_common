8566e1178826640298dca0d8c66a9b1df0cd11f5
ztc1997 <dev@ztc1997.com>
2024-11-12 23:31:44 +0800

    Revert "sysctl: Do not allow boosters to adjust swappiness"
    This reverts commit da90e27aaa0a738bba926496d18da572a54c6787.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -2748,15 +2748,6 @@ static struct ctl_table kern_table[] = {
 	{ }
 };
 
-bool task_is_booster(struct task_struct *tsk);
-
-static int proc_dointvec_minmax_ro_wrapper(struct ctl_table * table, int write, void __user * buffer, size_t * lenp, loff_t * ppos){
-	if (write && task_is_booster(current))
-		return 0;
-
-	return proc_dointvec_minmax(table, write, buffer, lenp, ppos);
-}
-
 static struct ctl_table vm_table[] = {
 	{
 		.procname	= "overcommit_memory",
@@ -2882,7 +2873,7 @@ static struct ctl_table vm_table[] = {
 		.data		= &vm_swappiness,
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax_ro_wrapper,
+		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_TWO_HUNDRED,
 	},
